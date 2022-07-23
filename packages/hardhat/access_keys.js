eb814c95657b6bcb424511d519aed5a6e30fc738
Nancy Stone <lpscrypt@gmail.com>
2022-07-23 03:53:04 +0200

    network swtich

--- packages/hardhat/access_keys.js
+++ packages/hardhat/access_keys.js
@@ -2,7 +2,7 @@ const infuraRinkeby =
 	"https://rinkeby.infura.io/v3/1609258d29dd4002beca55e5bb6bf73c";
 
 const rinkebyKey =
-	"132fe52e445127d8ff84148a5f0b1368aa190ba335f3f9c598079863c4f239f8";
+	"";
 
 const rinkebyContractAddress = "0x0bE718F6a4F2377F3Fc8d73609AF15B440c90128";
 
